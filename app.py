32a5ae1f99350941c862a134fd72f5cfa247efbc
CoilSFa <amirsite12@gmail.com>
2025-07-21 00:46:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask
 import threading
-import bot
+import bot  # Импорт файла bot.py
 
 app = Flask(__name__)
 
-threading.Thread(target=bot.main).start()
-
 @app.route("/")
-def home():
+def index():
     return "Bot is running!", 200
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+# Запуск бота в отдельном потоке
+threading.Thread(target=bot.main).start()
