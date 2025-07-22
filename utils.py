176f2a529ff187820547bff813a164c496375769
CoilSFa <amirsite12@gmail.com>
2025-07-23 00:46:52 +0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+import os
+from pydub import AudioSegment
+
+def convert_to_wav(input_path: str, output_path: str):
+    audio = AudioSegment.from_file(input_path)
+    audio.export(output_path, format="wav")
+    return output_path
