3e6e591c4296ce45118fec3c22be05e74dfc056e
CoilSFa <amirsite12@gmail.com>
2025-07-24 01:41:36 +0300

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -11,6 +11,7 @@ def generate_pdf(text: str) -> str:
     pdf.add_font("FreeSans", "", font_path, uni=True)
     pdf.set_font("FreeSans", size=12)
 
+
     # Разбиваем текст на строки и добавляем в PDF
     for line in text.split("\n"):
         pdf.multi_cell(0, 10, line)
