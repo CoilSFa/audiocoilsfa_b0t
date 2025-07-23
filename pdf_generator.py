459f60d94f7094e65d60937c809a6b530fae2e35
CoilSFa <amirsite12@gmail.com>
2025-07-23 13:45:17 +0300

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -5,14 +5,8 @@ import os
 def generate_pdf(text: str) -> str:
     pdf = FPDF()
     pdf.add_page()
-
-    # Добавляем шрифт, поддерживающий кириллицу
-    font_path = "FreeSans.ttf"
-    if not os.path.isfile(font_path):
-        raise Exception("Файл шрифта 'FreeSans.ttf' не найден. Помести его в корень проекта.")
-
-    pdf.add_font("FreeSans", "", font_path, uni=True)
-    pdf.set_font("FreeSans", size=12)
+    pdf.add_font('FreeSans', '', 'FreeSans.ttf', uni=True)
+    pdf.set_font('FreeSans', size=12)
 
     for line in text.split("\n"):
         pdf.multi_cell(0, 10, line)
