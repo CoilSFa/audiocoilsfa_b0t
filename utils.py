1b9407fcc6ccf6eeba41e11296f537edafae0ac9
CoilSFa <amirsite12@gmail.com>
2025-07-24 01:19:08 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 from pydub import AudioSegment
+import os
 
 def convert_to_wav(path: str) -> str:
-    output_path = path.replace(".ogg", ".wav")
+    output_path = path.rsplit(".", 1)[0] + ".wav"
     sound = AudioSegment.from_file(path)
     sound.export(output_path, format="wav")
     return output_path
