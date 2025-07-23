0096e403e4d803fe65b4b6095e2202d80a2c5278
CoilSFa <amirsite12@gmail.com>
2025-07-23 15:00:23 +0300

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,10 +1,14 @@
 from fpdf import FPDF
 import uuid
+import os
 
 def generate_pdf(text: str) -> str:
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size=12)  # Без add_font, Arial встроен
+
+    font_path = os.path.join(os.path.dirname(__file__), "FreeSans.ttf")
+    pdf.add_font("FreeSans", "", font_path, uni=True)
+    pdf.set_font("FreeSans", size=12)
 
     for line in text.split("\n"):
         pdf.multi_cell(0, 10, line)
